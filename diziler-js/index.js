d9d3d2fa6774def64fe50fff9479ca36adc1c867
Ahmet can Tanrıverdi <ahmetteymen72@gmail.com>
2025-11-06 16:43:11 +0300

    js-ders-reset

---
+++ diziler-js/index.js
@@ -0,0 +1,25 @@
+
+
+let dizi = ["koray","akmet","mehmet"];
+
+let resdult = dizi.shift();
+let resudlt = dizi.unshift();
+let resultf = dizi.concat(dizi2);
+let resuvlt = dizi.slice(1,3);
+let result = dizi.splice(1,2);
+console.log(dizi);
+console.log(result);
+
+let dizfi = [1,2,3,4,5,6,7,8,9];
+
+ let yeniDizi = dizi.filter((öğrenci) => öğrenci > 2);
+ let vaMi = dizi.includes(34);
+let varMi = dizi.indexOf(3);
+
+
+
+console.log(yeniDizi);
+
+
+
+
